ca78a7aa603f21461d9f0e11d1f7d161196399b6
Leonel Silvera <leonelsilvera9@gmail.com>
2025-07-09 09:44:44 -0300

    The "cursos" function and its constructor were removed

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,30 +9,14 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function __construct(private CourseRepositoryInterface $repository)
-    {
-        
-    }
-
     public function index()
     {
         $courses = Course::limit(9)->get();
+        
         return view("index", [
             "inicio" => true,
             "mostrar" => true,
             "courses" => $courses
-        ]);
-    }
-
-    public function cursos(Request $request)
-    {
-        $categories = Category::all();
-
-        $courses = $this->repository->courseSearchAndFilter($request);
-
-        return view("cursos", [
-            "courses" => $courses,
-            "categories" => $categories,
         ]);
     }
 }
