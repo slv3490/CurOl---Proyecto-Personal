c563590fdab721053095f88ec1ab4da0c7c6b078
Leonel Silvera <leonelsilvera9@gmail.com>
2025-09-17 08:21:45 -0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +9,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources'),
+        },
+    },
     build: {
         outDir: 'public/build',
         emptyOutDir: true,
