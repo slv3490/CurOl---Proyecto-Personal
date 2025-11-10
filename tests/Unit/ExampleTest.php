dd748a1446daf12525ef970bb1222cccc93d9abe
Leonel Silvera <leonelsilvera9@gmail.com>
2025-11-10 10:12:47 -0300

    Add UnitTest to fix error not found

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function basic_assertion_works()
+    {
+        $this->assertTrue(true);
+    }
+}
